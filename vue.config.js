d9c433cd8945ca69316168774f7f33a44bbf7351
lionnarta <lionnarta.savirandy2000@gmail.com>
2021-05-31 19:17:02 +0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
-module.exports = {
-    publicPath: '/vue-choropleth/'
-}
+// vue.config.js file to be place in the root of your repository
+
+// module.exports = {
+//     publicPath: process.env.NODE_ENV === 'production'
+//       ? '/vue-choropleth/'
+//       : '/'
+//   }
